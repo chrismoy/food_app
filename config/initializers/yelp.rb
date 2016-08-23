6f17a75dcd25a6f9264da4aae8dede1c84378a96
Chris Moy <chris@bigcorp.io>
2016-08-23 01:03:39 -0500

    update yelp initializer

--- config/initializers/yelp.rb
+++ config/initializers/yelp.rb
@@ -1,8 +1,15 @@
 require 'yelp'
 
+# Yelp.client.configure do |config|
+#   config.consumer_key = 'XmKClpSgglz94id2y1GmsQ'
+#   config.consumer_secret = 'S5BcdeGrfaHUgmC96ktfo7uBs5U'
+#   config.token = 'I5J3Kgnu3D-Vc2ZZvGIhtyWHe75XR2D7'
+#   config.token_secret = 'LpHdN38-Y9ImwWk4t-ONSiZrnlo'
+# end
+
 Yelp.client.configure do |config|
-  config.consumer_key = ENV['YELP_CONSUMER_KEY']
-  config.consumer_secret = ENV['YELP_CONSUMER_SECRET']
-  config.token = ENV['YELP_TOKEN']
-  config.token_secret = ENV['YELP_TOKEN_SECRET']
+  config.consumer_key = Figaro.env.YELP_CONSUMER_KEY
+  config.consumer_secret = Figaro.env.YELP_SECRET_KEY
+  config.token = Figaro.env.YELP_TOKEN
+  config.token_secret = Figaro.env.YELP_TOKEN_SECRET
 end
